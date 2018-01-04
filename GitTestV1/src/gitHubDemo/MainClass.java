6949796dee49cdaa1b7d826d832376e263a30e6f
akbr <akbr@akbr-HP-Stream-Laptop-14-ax0XX>
2018-01-04 13:45:28 -0500

    Add new Comments

--- GitTestV1/src/gitHubDemo/MainClass.java
+++ GitTestV1/src/gitHubDemo/MainClass.java
@@ -1,10 +1,15 @@
 package gitHubDemo;
 
-public class printDemo {
+public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is a test for GIT");
+		//Comments here
+		System.out.println("This is a test for GIT 1");
+		
+		
+		//Add new comments
+		System.out.println("ALLO");
 	}
 
 }
