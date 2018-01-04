94bec2f95e3efeb2b4867cef9bac927c23332ba1
akbr <akbr@akbr-HP-Stream-Laptop-14-ax0XX>
2018-01-04 13:10:22 -0500

    This is comment for main ()

--- GitTestV1/src/gitHubDemo/printDemo.java
+++ GitTestV1/src/gitHubDemo/printDemo.java
@@ -4,7 +4,7 @@ public class printDemo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("This is a test");
+		System.out.println("This is a test for GIT");
 	}
 
 }
